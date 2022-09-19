c2fd523a6b047b3e9034ecd5ad41672570c97b5c
AllLook <alet5289@gmail.com>
2022-09-20 01:22:34 +0300

    Создание методов и их применение

--- ArrayStr/Program.cs
+++ ArrayStr/Program.cs
@@ -1,17 +1,52 @@
 ﻿Console.WriteLine("Введите значение:");
-string a = Convert.ToString(Console.ReadLine()); 
+var a = Convert.ToString(Console.ReadLine());
 Console.WriteLine("Введите значение:");
-string b = Convert.ToString(Console.ReadLine()); 
+var b = Convert.ToString(Console.ReadLine());
 Console.WriteLine("Введите значение:");
-string c = Convert.ToString(Console.ReadLine());
- Console.WriteLine("Введите значение:");
-string d = Convert.ToString(Console.ReadLine());
- Console.WriteLine("Введите значение:");
-string e = Convert.ToString(Console.ReadLine()); 
-string[] array = new string [5];
+var c = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Введите значение:");
+var d = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Введите значение:");
+var e = Convert.ToString(Console.ReadLine());
+string[] array = new string[5];
 array[0] = a;
 array[1] = b;
 array[2] = c;
 array[3] = d;
 array[4] = e;
-string [] arr = new string[5];
+/* string [] arr = new string[5]; */ //новый массив на тоже кол во элементов
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]}\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+void Sorting(string[] array) // или тип метода string[]
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        /* for(int j = 0; j < arr.Length; j++) */
+        //{
+        // if(array[i].Length <= 3) 
+        // array[i] = arr[j]; // вот так не работает
+        if (array[i].Length <= 3)
+        {
+
+            Console.WriteLine($"{array[i]} ");
+        }
+
+        //}
+    }
+
+
+    /* return arr; */
+}
+
+PrintArray(array);
+Sorting(array);
+/* PrintArray(arr); */
