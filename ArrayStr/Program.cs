3c1f631cc8752c9dfc43b1a2a63bfed50d9a040d
AllLook <alet5289@gmail.com>
2022-09-19 21:51:36 +0300

    Создание массивов

---
+++ ArrayStr/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите значение:");
+string a = Convert.ToString(Console.ReadLine()); 
+Console.WriteLine("Введите значение:");
+string b = Convert.ToString(Console.ReadLine()); 
+Console.WriteLine("Введите значение:");
+string c = Convert.ToString(Console.ReadLine());
+ Console.WriteLine("Введите значение:");
+string d = Convert.ToString(Console.ReadLine());
+ Console.WriteLine("Введите значение:");
+string e = Convert.ToString(Console.ReadLine()); 
+string[] array = new string [5];
+array[0] = a;
+array[1] = b;
+array[2] = c;
+array[3] = d;
+array[4] = e;
+string [] arr = new string[5];
